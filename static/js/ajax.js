d7588e2e045d90e46d76c36bf4895722ff517203
c0rt <cyanat56@gmail.com>
2017-10-12 19:58:47 +0700

    Доработано окно добавления организатора.

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,16 +1,27 @@
 function nullToCurRole() {
-   $.get($("#inpt-null-role-url").val());
+    $.get($("#inpt-null-role-url").val());
 }
-$(document).ready( function () {
+
+$(document).ready(function () {
     var curRole = $("#inpt-change-role-cur").val();
     $(document).on("click", "a[name='role']", function () {
         var newRole = $(this).attr('id');
-        if(newRole !== curRole) {
+        if (newRole !== curRole) {
             $.post($("#inpt-change-role-url").val(), {newRole: newRole}, success);
         }
     });
 
     $('.modal').modal();
+
+    $('#addOrgSubmit').on('click', function () {
+        if ($('#olFio').hasClass('valid') && $('#olContact').hasClass('valid')) {
+            $('#modalAddOrg').modal('close');
+            $('#formNewOrg').submit();
+            Materialize.toast('Организатор добален!', 4000);
+        } else {
+            Materialize.toast('Ошибка при заполнении полей!', 4000);
+        }
+    });
 
     function success() {
         location.reload(true);
