efe39a54d533973ada4ad4e782ba498dd77269dc
c0rt <cyanat56@gmail.com>
2018-01-21 19:11:54 +0700

    clear application architecture

--- defaults.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-DEFAULT_QUALS = [u'б/р', u'IIIю', u'IIю', u'Iю', 'III', 'II', 'I', u'КМС', u'МС', u'МСМК', u'ЗМС']
--- up.sh
+++
@@ -1,1 +0,0 @@
-~/Документы/google-cloud-sdk/bin/dev_appserver.py --port=9000 /home/daria/tmmscw
